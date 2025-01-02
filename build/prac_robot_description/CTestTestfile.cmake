70677b154ff9855761029eaf865073eeac1e1e7a
Dolan Nimrod <dolannimrod@gmail.com>
2025-01-02 06:22:50 +0300

    my first urdf robot

---
+++ build/prac_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nimrod/ws_1/src/prac_robot_description
+# Build directory: /home/nimrod/ws_1/build/prac_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
