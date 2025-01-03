a13aa9acb716a753e0c76dbec8f2ebf9bc469f2a
Dolan Nimrod <dolannimrod@gmail.com>
2025-01-03 18:02:48 +0300

    I have added xacro to improve the URDF

--- build/prac_robot_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ build/prac_robot_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -55,3 +55,6 @@ endforeach()
 message(STATUS "Execute custom uninstall script")
 
 # begin of custom uninstall code
+
+# uninstall files installed using the symlink install functions
+include("/home/nimrod/ws_1/build/prac_robot_description/ament_cmake_symlink_install/ament_cmake_symlink_install_uninstall_script.cmake")
